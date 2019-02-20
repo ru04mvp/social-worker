13c1f3ca67481ad32196be45232d721c31f3f16f
jake <ru04mvp@gmail.com>
2019-02-20 14:30:46 +0800

    [ 預設畫面 ] 直接帶入系統運作狀態

--- backend/application/controllers/Welcome.php
+++ backend/application/controllers/Welcome.php
@@ -3,23 +3,18 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Welcome extends CI_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
-	public function index()
-	{
-		$this->load->view('welcome_message');
-	}
+    public function __construct() {
+        parent::__construct();
+        #載入必Load項目
+        $this->load->helper("Common_Helper");
+    }
+
+    /**
+     * 返回系統線上狀態
+     *
+     * @return void
+     */
+    public function index() {
+        json_respon(['code' => '0000', 'message' => 'system is work.']);
+    }
 }
