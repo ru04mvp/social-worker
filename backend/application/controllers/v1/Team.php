7776201f980965803834f476d5042e7fd011bccc
jake <ru04mvp@gmail.com>
2019-02-20 14:31:27 +0800

    [ 團隊 ] 增加功能架構

---
+++ backend/application/controllers/v1/Team.php
@@ -0,0 +1,60 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Team extends CI_Controller {
+
+    /**
+     * 取得團隊列表資料
+     *
+     * @return void
+     */
+    public function getList() {
+
+    }
+
+    /**
+     * 取得團隊細節資料
+     *
+     * @return void
+     */
+    public function getDetail() {
+
+    }
+
+    /**
+     * 創建團隊資料
+     *
+     * @return void
+     */
+    public function creatDetail() {
+
+    }
+
+    /**
+     * 更新團隊資料
+     *
+     * @return void
+     */
+    public function updateDetail() {
+
+    }
+
+    /**
+     * 移除團隊資料
+     *
+     * @return void
+     */
+    public function deleteDetail() {
+
+    }
+
+    /**
+     * 設定當前團隊
+     *
+     * @return void
+     */
+    public function setType() {
+
+    }
+
+}
