3617559c73e0cfcd74a4d82b332e1a47bf1324c4
jake <ru04mvp@gmail.com>
2019-02-20 14:31:19 +0800

    [ 會員 ] 增加功能架構

---
+++ backend/application/controllers/v1/Member.php
@@ -0,0 +1,69 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Member extends CI_Controller {
+
+    /**
+     * 取得會員列表資料
+     *
+     * @return void
+     */
+    public function getList() {
+
+    }
+
+    /**
+     * 取得會員細節資料
+     *
+     * @return void
+     */
+    public function getDetail() {
+
+    }
+
+    /**
+     * 創建會員資料
+     *
+     * @return void
+     */
+    public function creatDetail() {
+
+    }
+
+    /**
+     * 更新會員資料
+     *
+     * @return void
+     */
+    public function updateDetail() {
+
+    }
+
+    /**
+     * 移除會員資料
+     *
+     * @return void
+     */
+    public function deleteDetail() {
+
+    }
+
+    /**
+     * 登入會員
+     *
+     * @return void
+     */
+    public function login() {
+
+    }
+
+    /**
+     * 登出會員
+     *
+     * @return void
+     */
+    public function logout() {
+
+    }
+
+}
