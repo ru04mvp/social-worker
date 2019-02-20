4926a94def2cce3f551f7898c58b3e9caff806bb
jake <ru04mvp@gmail.com>
2019-02-20 14:31:09 +0800

    [ 頻道 ] 增加功能架構

---
+++ backend/application/controllers/v1/Channel.php
@@ -0,0 +1,60 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Channel extends CI_Controller {
+
+    /**
+     * 取得頻道列表資料
+     *
+     * @return void
+     */
+    public function getList() {
+
+    }
+
+    /**
+     * 取得頻道細節資料
+     *
+     * @return void
+     */
+    public function getDetail() {
+
+    }
+
+    /**
+     * 創建頻道資料
+     *
+     * @return void
+     */
+    public function creatDetail() {
+
+    }
+
+    /**
+     * 更新頻道資料
+     *
+     * @return void
+     */
+    public function updateDetail() {
+
+    }
+
+    /**
+     * 移除頻道資料
+     *
+     * @return void
+     */
+    public function deleteDetail() {
+
+    }
+
+    /**
+     * 設定當前頻道
+     *
+     * @return void
+     */
+    public function setType() {
+
+    }
+
+}
