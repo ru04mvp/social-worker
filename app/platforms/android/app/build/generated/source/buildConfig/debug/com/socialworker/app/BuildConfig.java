6024213a86438b075b6e051136399c685c5297e7
jake <ru04mvp@gmail.com>
2019-03-05 16:41:22 +0800

    [ 整合測試 ] 增加整合測試用版本

---
+++ app/platforms/android/app/build/generated/source/buildConfig/debug/com/socialworker/app/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.socialworker.app;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.socialworker.app";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 10900;
+  public static final String VERSION_NAME = "1.9";
+}
